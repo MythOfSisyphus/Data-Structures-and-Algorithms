f9f13a4f71acfdf3340f07357697d10c8df50f18
Lucky <luckychouhan369@gmail.com>
2024-03-13 23:29:01 +0530

    Palindrom And N_Queens file added

---
+++ Algorithms Book/Backtracking/N_queens.cpp
@@ -0,0 +1,14 @@
+/*
+The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two queens attack each other.
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    
+
+    return 0;
+}
---
+++ Essential Stuff/Recursion_Basics/Palindrom_Checker.cpp
@@ -0,0 +1,20 @@
+/*
+Write a recursive function to check if a given string is a palindrome.
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool CheckingIt(char[] input){
+    int input_len = strlen(input);
+    return true;
+}
+
+int main(){
+
+    char lucky[] = "useful";
+    int x = strlen(lucky);
+
+    return 0;
+}
