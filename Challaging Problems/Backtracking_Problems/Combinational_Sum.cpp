09a715e5745e016970a86a5683fbdc0a0a4b2de4
Lucky <luckychouhan369@gmail.com>
2024-08-02 20:15:03 +0530

    Put it into TODO

--- Challaging Problems/Backtracking_Problems/Combinational_Sum.cpp
+++ Challaging Problems/Backtracking_Problems/Combinational_Sum.cpp
@@ -21,30 +21,7 @@ using namespace std;
 
 int main(){
 
-    int arr[] = {2, 4, 6, 8};
-
-    int i=0, j=0, sum=arr[0], x=8;
-    int k=0;
-
-    while(i < 3){
-        while(sum <= x){
-            for(int h=0; h<k; h++){
-                cout<<arr[i-1];
-            }
-            cout<<arr[i]<<" ";
-            sum += arr[i];
-            j++;
-        }
-
-        cout<<"\n";
-
-        i++;
-        if(j > 2){
-            sum = sum - (3*arr[i-1]) + arr[i];
-            j=0;
-        }
-    }
-    
+    // TODO
 
     return 0;
 }
