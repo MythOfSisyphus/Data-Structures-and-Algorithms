864aeec7ce9d0802b6ec711b31ae0397c0d06d98
Lucky <luckychouhan369@gmail.com>
2024-08-02 21:49:49 +0530

    Done

--- Essential Stuff/Recursion_Basics/Palindrom_Checker.cpp
+++ Essential Stuff/Recursion_Basics/Palindrom_Checker.cpp
@@ -1,20 +1,47 @@
 /*
 Write a recursive function to check if a given string is a palindrome.
+0 1 2 3 4
 */
 
 #include<bits/stdc++.h>
 
 using namespace std;
 
-bool CheckingIt(char[] input){
-    int input_len = strlen(input);
+bool isPalindrome(string S){
+    int S_length = S.length();
+
+    if(S_length % 2 == 0){
+        for(int i=0; i<S_length/2; i++){
+            if(S[i] != S[S_length-(i+1)])
+                return false;
+        }
+    }
+
+    if(S_length % 2 == 1){
+        for(int i=0; i<(S_length/2 - 1); i++){
+            if(S[i] != S[S_length-(i+1)])
+                return false;
+        }
+    }
+
     return true;
 }
 
 int main(){
 
-    char lucky[] = "useful";
-    int x = strlen(lucky);
+    string name = "lucky";
+
+    // cout<<name<<endl;
+    // cout<<name[1]<<endl;
+    // int n = name.length();
+    // cout<<n<<endl;
+    // cout<<name;
+
+    cout<<isPalindrome(name);
+    cout<<isPalindrome("Printf");
+    cout<<isPalindrome("AKA");
+    cout<<isPalindrome("racecar");
+    cout<<isPalindrome("Palindrome");
 
     return 0;
 }
