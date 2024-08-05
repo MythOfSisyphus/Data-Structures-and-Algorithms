ed593bc7c62a179ecb66cf73a1a07757b1c5af10
Lucky <luckychouhan369@gmail.com>
2024-08-05 11:56:02 +0530

    MaxDepth Done :) with explanation

--- Challaging Problems/BinaryTrees_Problems/Maximum_depth.cpp
+++ Challaging Problems/BinaryTrees_Problems/Maximum_depth.cpp
@@ -1,7 +1,5 @@
 /*
-For Problem's Statement see: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
-
-// TODO
+For Problem's Statement see: https://www.geeksforgeeks.org/problems/maximum-depth-of-binary-tree/1
 */
 
 #include<bits/stdc++.h>
@@ -16,27 +14,86 @@ struct Tree {
     Tree(int value): data(value), left(nullptr), right(nullptr){}
 };
 
-int MaxDepth(Tree* x, int d = 0){
-    // if(x->left != nullptr){
-    //     d+=1;
-    //     MaxDepth(x->left, d);
-    // }
-    if(x->right != nullptr){
-        d = d + 1;
-        MaxDepth(x->right, d);
-    }
+/*
+Remember the fact that the following function counts the number of nodes in a binary tree.
 
-    return d;
+UNDERSTANDING THE MAXDEPTH FUNCTION: To explain this function I'm gonna consider a binary tree
+with only two nodes code for which I have written after int main function see there.
+
+So Call the MaxDepth(root) where root is 1.
+our root node is not NULL, so proceed
+now call the MaxDepth(root->left) which is 2.
+and again 2 is not NULL, so proceed,
+now call the MaxDepth(root->left->left) which is NULL, so it return 0,
+now call the MaxDepth(root->left->right) which is again NULL, so it also return 0,
+so for MaxDepth(root->left) = 1
+
+now we call MaxDepth(root->right) which is null so it returns 0
+
+now we can get our reuired answer which is 2 by doing last step of code.
+
+*/
+int MaxDepth(Tree* x){
+    // if node is null
+    if(x == NULL)
+        return 0;
+
+    // getting the left depth
+    int left = MaxDepth(x->left);
+
+    // getting the right depth
+    int right = MaxDepth(x->right);
+
+    return max(left, right) + 1;
+}
+
+// Just a recursive function to clean up memeory
+void DeleteNodes(Tree* x){
+    if(x != NULL){
+        // Recursively delete the left nodes
+        DeleteNodes(x->left);
+        // Recursively delete the right nodes
+        DeleteNodes(x->right);
+        // delete the current node
+        delete x;
+    }
 }
 
 int main(){
 
-    Tree* john = new Tree(1);
-    john->right = new Tree(3);
-    john->right->right = new Tree(7);
-    john->right->right->right = new Tree(11);
+    Tree* test = new Tree(1);
+    test->left = new Tree(2);
 
-    cout<<MaxDepth(john);
+    cout<<MaxDepth(test)<<endl;
+
+    DeleteNodes(test);
+
+    // Tree* john = new Tree(1);
+    // john->right = new Tree(3);
+    // john->right->right = new Tree(7);
+    // john->right->right->right = new Tree(11);
+
+    // cout<<MaxDepth(john)<<endl;
+
+    // DeleteNodes(john);
+
+    // Tree* faults = new Tree(1);
+    // faults->left = new Tree(2);
+
+    // faults->left->right = new Tree(3);
+    // faults->left->right->left = new Tree(4);
+
+    // faults->left->right->left->left = new Tree(8);
+    // faults->left->right->left->left->right = new Tree(9);
+    // faults->left->right->left->left->right->right = new Tree(10);
+
+    // faults->left->right->right = new Tree(5);
+    // faults->left->right->right->left = new Tree(7);
+    // faults->left->right->right->right = new Tree(6);
+
+    // cout<<MaxDepth(faults)<<endl;
+
+    // DeleteNodes(faults);
 
     return 0;
 }
